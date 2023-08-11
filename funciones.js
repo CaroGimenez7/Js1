032747b3342ff2b67c9abf1c9a895a569cd9e392
Carolina Gimenez <gimenezcarolina909@gmail.com>
2023-08-10 22:08:27 -0300

    agregue ejercicios de funciones

--- funciones.js
+++ funciones.js
@@ -1,3 +1,4 @@
+//todo DRY: REGLA QUE SIGNIFICA NO REPITAS TU CODIGO
 //? FUNCIONES DECLARATIVAS
 
 // function hello() {
@@ -20,31 +21,45 @@
 
 //! EJERCICIO 1: BASE DE DATOS PARA ELIMINAR USUARIOS:
 
-let database = [`matias`, `celeste`, `agustina`, `mario`, `joaquin`, `lucas`, `ivan`];
+// let database = [`matias`, `celeste`, `agustina`, `mario`, `joaquin`, `lucas`, `ivan`];
 
-const deleteUser = () => {
-     let nameUser = prompt(`ingrese el usuario que desea eliminar`).toLocaleLowerCase();
+// const deleteUser = () => {
+//      let nameUser = prompt(`ingrese el usuario que desea eliminar`).toLocaleLowerCase();
 
-     let position = database.indexOf(nameUser);
-     if (database.includes(nameUser)) {
-        database.splice(position,1);
-        alert(`usuario eliminado correctamente!`);
-     }else {
-        alert(`usuario no encontrado!`)
-     }
+//      let position = database.indexOf(nameUser);
+//      if (database.includes(nameUser)) {
+//         database.splice(position,1);
+//         alert(`usuario eliminado correctamente!`);
+//      }else {
+//         alert(`usuario no encontrado!`)
+//      }
 
      
+// }
+//! EJERCICIO 1: BASE DE DATOS PARA EDITAR USUARIOS:
+
+//  const updateUser = () => {
+//    let nameUser = prompt(`ingrese el usuario que desea eliminar`).toLocaleLowerCase();
+//    let update = prompt(`que usuario queres agregar`).toLocaleLowerCase();
+//    let position = database.indexOf(nameUser);
+
+//    if (database.includes(nameUser)) {
+//       database.splice(position,1,update);
+//       alert(`usuario editado correctamente`)
+//    }else {
+//       alert(`usuario no encontrado!`)
+//    }
+//  }
+
+//? LAS FUNCIONES CALLBACK SON FUNCIONES QUE SE PASAN COMO ARGUMENTO DE OTRAS FUNCIONES
+
+const hi = nombre => alert(`hola` + nombre);
+const bye = nombre => alert(`chau ` + nombre)
+function userRegister (saludo) {
+    const name = prompt(`como es tu nombre?`);
+    saludo(name)
 }
+userRegister(hi);
+userRegister(bye);
 
- const updateUser = () => {
-   let nameUser = prompt(`ingrese el usuario que desea eliminar`).toLocaleLowerCase();
-   let update = prompt(`que usuario queres agregar`).toLocaleLowerCase();
-   let position = database.indexOf(nameUser);
-
-   if (database.includes(nameUser)) {
-      database.splice(position,1,update);
-      alert(`usuario editado correctamente`)
-   }else {
-      alert(`usuario no encontrado!`)
-   }
- }
+//? METODOS FILTER, FIND, MAP Y FOREACH 
