58acc8427aa561d3322c98e246f3eadd0d923def
Carolina Gimenez <gimenezcarolina909@gmail.com>
2023-08-21 01:07:08 -0300

    objetos

--- carrito.js
+++ carrito.js
@@ -1,41 +1,41 @@
-let products = [`gaseosa`, `agua`, `cerveza`, `vino`, `fernet`, `gin`, `vodka`];
-let carrito = [];
+// let products = [`gaseosa`, `agua`, `cerveza`, `vino`, `fernet`, `gin`, `vodka`];
+// let carrito = [];
 
-const agregarProducto = () => {
-   const productoAgregado = prompt(`que producto desea llevar?`).toLocaleLowerCase()
-   if (products.includes(productoAgregado)) {
-    carrito.push(productoAgregado);
-    alert(`producto agregado!`)
-   }else{
-    alert(`no vendemos ese producto, volove la proxima!`)
-   }
-}
+// const agregarProducto = () => {
+//    const productoAgregado = prompt(`que producto desea llevar?`).toLocaleLowerCase()
+//    if (products.includes(productoAgregado)) {
+//     carrito.push(productoAgregado);
+//     alert(`producto agregado!`)
+//    }else{
+//     alert(`no vendemos ese producto, volove la proxima!`)
+//    }
+// }
 
-const mostrarCarrito = () => {
-   alert(`tus productos seleccionados son: ` + carrito.join(`\n `)) // \n hace un salto de linea
+// const mostrarCarrito = () => {
+//    alert(`tus productos seleccionados son: ` + carrito.join(`\n `)) // \n hace un salto de linea
 
-}
+// }
 
-const buscarUnProducto = () => {
-   const producABuscar = prompt(`ingrese el producto que desea buscar`).toLocaleLowerCase()
-   const productoEncontrado = carrito.find(producto => producto == producABuscar )
-   alert(productoEncontrado)
+// const buscarUnProducto = () => {
+//    const producABuscar = prompt(`ingrese el producto que desea buscar`).toLocaleLowerCase()
+//    const productoEncontrado = carrito.find(producto => producto == producABuscar )
+//    alert(productoEncontrado)
 
-}
+// }
 
-const buscarcoincidencias = () => {
-   const buscar = prompt(`buscar coincidencias`).toLocaleLowerCase();
-   const resultado = products.filter(producto => producto.includes(buscar));
-   alert(resultado)
-}
+// const buscarcoincidencias = () => {
+//    const buscar = prompt(`buscar coincidencias`).toLocaleLowerCase();
+//    const resultado = products.filter(producto => producto.includes(buscar));
+//    alert(resultado)
+// }
 
-const eliminarProducto = () => {
-   productoAEliminar = prompt(`ingrese el producto que desea eliminar`);
-   const productoSeleccionado = carrito.indexOf(productoAEliminar)
-   if (carrito.includes(productoAEliminar)) {
-      carrito.splice(productoSeleccionado, 1)
-      alert(`producto eliminado correctamente`)
-   }else{
-      alert(`producto no encontrado`)
-   }
-}
+// const eliminarProducto = () => {
+//    productoAEliminar = prompt(`ingrese el producto que desea eliminar`);
+//    const productoSeleccionado = carrito.indexOf(productoAEliminar)
+//    if (carrito.includes(productoAEliminar)) {
+//       carrito.splice(productoSeleccionado, 1)
+//       alert(`producto eliminado correctamente`)
+//    }else{
+//       alert(`producto no encontrado`)
+//    }
+// }
---
+++ objetos.js
@@ -0,0 +1,20 @@
+let person = {
+    name: `matias`,
+    age: 30,
+    location: `San Miguel de Tucuman`,
+    hobbies: [`andar en bici`, `programar`],
+    pet: [
+        {
+            name: `firulais`,
+            type: `dog`
+        },
+        {
+            name: `Blanco`,
+            type: `cat`
+        }
+    ],
+    children: `no`,
+    saludar: function () {
+        console.log(`hola chicos soy Matias`)
+    }
+}
